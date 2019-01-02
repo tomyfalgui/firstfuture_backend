74dd90392c27140c439129ef241ea8c1569d2a13
brenmat <brennanmato@gmail.com>
2019-01-02 16:54:28 +0800

    Passport strategies code quality improved

--- passport.js
+++ passport.js
@@ -1,78 +1,72 @@
 const passport = require('passport');
 const passportJWT = require("passport-jwt");
 const LocalStrategy = require('passport-local').Strategy;
-const {User,Company} = require('./database.js');
+const { User, Company } = require('./database.js');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv').config();
 const ExtractJWT = passportJWT.ExtractJwt;
-const JWTStrategy   = passportJWT.Strategy;
+const JWTStrategy = passportJWT.Strategy;
 
-// local 
-passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password'
-    }, 
-    function (e, p, cb) {
-        return User.findOne({where:{email:e}}).then(user => {
-               if (!user) {
-                   return cb(null, false, {message: 'Incorrect email or password.'});
-               }
-               bcrypt.compare(p,user.password,function(err,res){
-                    if(res){
-                        return cb(null, {id:user.id}, {message: 'Logged In Successfully'});
-                    }
-                    return cb(null, false, {message: 'Incorrect email or password.'});
-               });
-            }).catch(err => cb(err));
+const jwtSettings = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWTSecret
+}
+
+const localAuthFields = {
+    usernameField: 'email',
+    passwordField: 'password'
+}
+
+passport.use('local', new LocalStrategy(localAuthFields,
+    function (candidateEmail, candidatePassword, callback) {
+        return User.findOne({ where: { email: candidateEmail } }).then(user => {
+            if (!user) {
+                return callback(null, false, { message: 'Incorrect email or password.' });
+            }
+            return validatePassword(candidatePassword, user, callback);
+        }).catch(err => callback(err));
     }
 ));
 
-// jwt
-passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey   : process.env.JWTSecret
-},
-function (jwtPayload, cb) {
-    return User.findById(jwtPayload.id)
-        .then(user => {
-            return cb(null, user);
-        })
-        .catch(err => {
-            return cb(err);
-        });
-}
+passport.use('company-local', new LocalStrategy(localAuthFields,
+    function (candidateEmail, candidatePassword, callback) {
+        return Company.findOne({ where: { email: candidateEmail } }).then(user => {
+            if (!user) {
+                return callback(null, false, { message: 'Incorrect email or password.' });
+            }
+            return validatePassword(candidatePassword, user, callback);
+        }).catch(err => callback(err));
+    }
 ));
 
-passport.use('company-local', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-}, 
-function (e, p, cb) {
-    return Company.findOne({where:{email:e}}).then(user => {
-           if (!user) {
-               return cb(null, false, {message: 'Incorrect email or password.'});
-           }
-           bcrypt.compare(p,user.password,function(err,res){
-                if(res){
-                    return cb(null, {id:user.id}, {message: 'Logged In Successfully'});
-                }
-                return cb(null, false, {message: 'Incorrect email or password.'});
-           });
-        }).catch(err => cb(err));
-}
+passport.use('jwt', new JWTStrategy(jwtSettings,
+    function (jwtPayload, callback) {
+        return User.findById(jwtPayload.id)
+            .then(user => {
+                return callback(null, user);
+            })
+            .catch(err => {
+                return callback(err);
+            });
+    }
 ));
 
-passport.use('company-jwt', new JWTStrategy({
-jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-secretOrKey   : process.env.JWTSecret
-},
-function (jwtPayload, cb) {
-return Company.findById(jwtPayload.id)
-    .then(user => {
-        return cb(null, user);
-    })
-    .catch(err => {
-        return cb(err);
+passport.use('company-jwt', new JWTStrategy(jwtSettings,
+    function (jwtPayload, callback) {
+        return Company.findById(jwtPayload.id)
+            .then(user => {
+                return callback(null, user);
+            })
+            .catch(err => {
+                return callback(err);
+            });
+    }));
+
+function validatePassword(candidate, user, callback) {
+    bcrypt.compare(candidate, user.password, function (err, res) {
+        if (res) {
+            return callback(null, { id: user.id }, { message: 'Logged In Successfully' });
+        }
+        return callback(null, false, { message: 'Incorrect email or password.' });
     });
 }
-));

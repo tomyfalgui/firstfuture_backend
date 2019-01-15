0b99a3b42e7b2a7392bb6f43672f5ff9780da431
Henry Gonzales <henry.b.gonzales@obf.ateneo.edu>
2019-01-15 12:30:43 +0800

    removed unnecessary code in email-parser

--- test/email-parser.js
+++ test/email-parser.js
@@ -1,12 +1,12 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-var Imap = require("imap");
-var MailParser = require("mailparser").MailParser;
-var Promise = require("bluebird");
-require('dotenv').config({path: '../.env'});
+const Imap = require("imap");
+const MailParser = require("mailparser").MailParser;
+const Promise = require("bluebird");
+require('dotenv').config({ path: '../.env' });
 Promise.longStackTraces();
 
-var imapConfig = {
+const imapConfig = {
     user: process.env.MAILER_SERVICE_USER,
     password: process.env.MAILER_SERVICE_PASSWORD,
     host: 'imap.gmail.com',
@@ -14,7 +14,7 @@ var imapConfig = {
     tls: true
 };
 
-var imap = new Imap(imapConfig);
+const imap = new Imap(imapConfig);
 Promise.promisifyAll(imap);
 
 imap.once("ready", execute);
@@ -36,16 +36,7 @@ function execute() {
                 imap.end();
                 return;
             }
-            /* mark as seen
-            imap.setFlags(results, ['\\Seen'], function(err) {
-                if (!err) {
-                    console.log("marked as read");
-                } else {
-                    console.log(JSON.stringify(err, null, 2));
-                }
-            });*/
-
-            var f = imap.fetch(results, { bodies: "" });
+            let f = imap.fetch(results, { bodies: "" });
             f.on("message", processMessage);
             f.once("error", function(err) {
                 return Promise.reject(err);
@@ -60,26 +51,15 @@ function execute() {
 
 
 function processMessage(msg, seqno) {
-    //console.log("Processing msg #" + seqno);
-    // console.log(msg);
-
-    var parser = new MailParser();
-
+    let parser = new MailParser();
 
     parser.on('data', data => {
         if (data.type === 'text') {
-            // console.log(seqno);
             console.log(data.text.slice(
                 data.text.indexOf('[') + 1,
                 data.text.indexOf(']')
-            )); /* data.html*/
+            ));
         }
-
-        // if (data.type === 'attachment') {
-        //     console.log(data.filename);
-        //     data.content.pipe(process.stdout);
-        //     // data.content.on('end', () => data.release());
-        // }
     });
 
     msg.on("body", function(stream) {
@@ -88,7 +68,6 @@ function processMessage(msg, seqno) {
         });
     });
     msg.once("end", function() {
-        // console.log("Finished msg #" + seqno);
         parser.end();
     });
 }
